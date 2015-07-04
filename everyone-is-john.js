13e153a972387b8e46c3de9261f69b395b0ff09b
Andrew McPherson <andrewmcp333@gmail.com>
2015-07-04 12:57:57 -0500

    Added interaction.

--- everyone-is-john.js
+++ everyone-is-john.js
@@ -2,87 +2,97 @@ var Games = new Mongo.Collection("games")
 var Users = new Mongo.Collection("users")
 
 if(Meteor.isClient) {
-    var user = Users.insert({})
-    Session.set("user", user)
+    var user_id = Users.insert({})
+    Session.set("user_id", user_id)
 
-    Router.route("/", function() {
-        this.render("home")
-    })
-    Router.route("/play", function() {
-        this.render("new_game")
-    })
-    Router.route("/play/:code", function() {
-        var game_code = this.params.code
+    Router.route("/game/:game_code", function() {
+        var game_code = this.params.game_code
         var game = Games.findOne({
             "code": game_code
         })
-        if(game == undefined) {
-            this.render("new_game", {
-                "data": {
-                    "code": game_code
+        var user_id = Session.get("user_id")
+        var user = Users.findOne({
+            "_id": user_id
+        })
+
+        if(!game) {
+            this.render("nogame", {
+                data: {
+                    "game_code": game_code
                 }
             })
+        } else if(game.master == user_id) {
+            Router.go("game/:game_code/gm", {
+                "game_code": game_code
+            })
         } else {
-            this.render("game", {
-                "data": Games.findOne({
-                    "code": game_code
-                })
+            Router.go("game/:game_code/voice", {
+                "game_code": game_code
             })
         }
-    })
+    }, {name: "game/:game_code"})
+
+    Router.route("/game/:game_code/gm", function() {
+        var game_code = this.params.game_code
+        var game = Games.findOne({
+            "code": game_code
+        })
+        var user_id = Session.get("user_id")
+        var user = Users.findOne({
+            "_id": user_id
+        })
+
+        if(game.master == user_id) {
+            this.render("master")
+        } else {
+            Router.go("game/:game_code")
+        }
+    }, {name: "game/:game_code/gm"})
+
+    Router.route("/game/:game_code/voice", function() {
+        var game_code = this.params.game_code
+        var game = Games.findOne({
+            "code": game_code
+        })
+        var user_id = Session.get("user_id")
+        var user = Users.findOne({
+            "_id": user_id
+        })
+        var voice = game.voices[game.voices.indexOf(user_id)]
+        if(!voice) {
+            //make a new user
+        } else {
+            Router.go("game/:game_code/voice/:voice_code", {
+                "game_code": game_code,
+                "voice_code": voice.code
+            })
+        }
+    }, {name: "game/:game_code/voice"})
+
+    Router.route("/game/:game_code/voice/:voice_code", function() {
+        var game_code = this.params.game_code
+        var voice_code = this.params.voice_code
+        this.render("voice", {
+            "game_code": game_code,
+            "voice_code": voice_code
+        })
+    }, {name: "game/:game_code/voice/:voice_code"})
 
     Template.registerHelper("print", function() {
         console.log(this)
-    })
-
-    Template.game.events({
-        "submit #chat": function(event) {
-            event.preventDefault()
-            var author = Session.get("user")
-            var message = event.target.message.value
-            event.target.message.value = new String()
-            Games.update(this._id, {
-                "$push": {
-                    "chats": {
-                        "author": author,
-                        "message": message
-                    }
-                }
-            })
-        }
-    })
-
-    Template.new_game.events({
-        "submit form": function(event) {
-            event.preventDefault()
-            var game_master = Session.get("user")
-            var game_name = event.target.name.value
-            var game_code = game_name.toLowerCase()
-            var game = Games.findOne({
-                "code": game_code
-            })
-            if(game != undefined) {
-                alert(game_name + " already exists")
-            } else {
-                Router.go("/play/" + game_code)
-                Games.insert({
-                    "code": game_code,
-                    "name": game_name,
-                    "chats": new Array(),
-                    "master": game_master
-                })
-            }
-        }
     })
 }
 
 if(Meteor.isServer) {
     Meteor.startup(function() {
+        Users.remove({})
         Games.remove({})
         Games.insert({
-            "name": "Awesomesauce",
+            "name": "Adventures of Awesomesauce",
             "code": "awesomesauce",
-            "master": "PM9LKALL2ATCY27VV",
+            "users": {
+                "master": "PM9LKALL2ATCY27VV",
+            },
             "chats": new Array()
         })
     })
